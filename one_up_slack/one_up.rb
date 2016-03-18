875a3a3c25fc793d765dc9be7194b53f23712418
parasquid <tristan.gomez@gmail.com>
2016-03-18 20:22:58 +0800

    sample payload

--- one_up_slack/one_up.rb
+++ one_up_slack/one_up.rb
@@ -3,3 +3,15 @@ require 'sinatra'
 get '/' do
   'Hello world!'
 end
+
+post "/incoming" do
+  content_type :json
+  {
+    "text" => "It's 80 degrees right now.",
+    "attachments" => [
+        {
+            "text" => "Partly cloudy today and tomorrow"
+        }
+    ]
+  }.to_json
+end
