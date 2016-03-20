9363b00187cabdbcfd977bc45fb49d42fc1974f6
parasquid <tristan.gomez@gmail.com>
2016-03-20 17:41:29 +0800

    cleanup the entry hash

--- spec/one_up/ledger_spec.rb
+++ spec/one_up/ledger_spec.rb
@@ -7,44 +7,28 @@ describe OneUp::Ledger do
   Given(:receiver) { double("Receiver") }
   Given(:gift) { double("Gift") }
   Given(:message) { "this is the message" }
+  Given(:entry) {
+   { giver: giver, receiver: receiver, gift: gift, message: message }
+  }
 
   context "can add an entry" do
-    When { ledger.add_entry(giver: giver, receiver: receiver, gift: gift, message: message) }
+    When { ledger.add_entry(entry) }
     Then { expect(ledger.entries.first)
-            .to include(
-              {
-                giver: giver,
-                receiver: receiver,
-                gift: gift,
-                message: message
-              }
-            )
+            .to include(entry)
     }
   end
 
   context "stores the time of record creation" do
     Given(:created_at) { Time.at 0 }
-    When { ledger.add_entry(
-            giver: giver,
-            receiver: receiver,
-            gift: gift,
-            message: message,
-            created_at: created_at)
+    When { ledger.add_entry(entry.merge(created_at: created_at))
     }
     Then { expect(ledger.entries.first)
-            .to include(
-              giver: giver,
-              receiver: receiver,
-              gift: gift,
-              message: message,
-              created_at: created_at)
+            .to include(entry.merge(created_at: created_at))
     }
   end
 
   context "can list all entries" do
-    When { 3.times {ledger.add_entry(
-      giver: giver, receiver: receiver, gift: gift, message: message
-    )} }
+    When { 3.times {ledger.add_entry(entry)} }
     Then { ledger.entries.count == 3 }
   end
 end
